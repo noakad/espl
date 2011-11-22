5f5eb5d968365b7757060ffccea26213fffba178
Noa Kadmon <noa.kadmon@gmail.com>
2011-11-22 22:28:03 +0200

    added Lab3

---
+++ Lab3/bfix.c
@@ -0,0 +1,85 @@
+/* NAME
+   bfix - apply differences to a binary file
+
+SYNOPSIS
+   bfix [OPTIONS] file differences
+
+DESCRIPTION
+   bfix applies the differences (second argument) as printed
+   by bcmp(3) to the file (the first argument).
+   Before applying each difference, bfix verifies that the old value
+   of the byte in the file is the same as the old value in the
+   difference line. If option -r is specified, the differences are
+   reversed: the new value must be changed to the old value.
+
+OPTIONS
+   -h  print a summary of options and exit
+   -r  reverse the differences
+   -m  print a message each time a change is applied
+
+SEE ALSO
+    bcmp(3)
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include "diff.h"
+
+
+int main (int argc , char** argv)
+{
+
+	int i,c1,f;
+	FILE* tbchanged;
+	FILE* diffs;
+	char* buff;
+	struct diff *diff;
+
+	tbchanged = fopen(argv[1],"+r");//opening the file we want to cange the bytes in
+	if(!tbchanged){
+	  printf("ERROR: faid to open tbchanged \n");
+	}
+	diffs = fopen(argv[2],"r");//openingthe list of differences
+	if(!diffs){
+	  printf("ERROR: faid to open diffs \n");
+	}
+
+	c1 = fgetc(diffs);
+	while(c1 != EOF){
+	  i = 1;
+	  while((c1)!= 0){//read one line
+	    buff[i] = c1;
+	    c1 = fgetc(diffs);
+	    ++i;
+	  }
+	  parsediff(buff, diff);
+	  fseek(tbchanged, (long)diff->offset, SEEK_SET);
+	  if(fgetc(tbchanged)==(diff->old)){
+	    if((fputc(diff->new, tbchanged))==(diff->new)){
+	   //if((fwrite(diff->new, 1, 1, tbchanged))==1){
+	      fputdiff(stdout, diff);
+	    }
+	  }
+	  else{
+	    printf("WRONG INFORMATION IN DIFFS\n");
+	  }
+	  
+	  c1 = fgetc(diffs);
+	}//while
+  fclose(tbchanged);
+  fclose(diffs);
+  return 0;
+}
+   
+
+
+
+
+//parsediff("byte 33 -174 +230", &diff);//לא משנה מה "פארסדיף" מחזירה לי - ברגע שהשתמשתי בה, זה יוצר לי "סטראקט" -דיף ומאתחל לי את השדות להיות מה שביקשתי בקריאה לפונקצי זו. ככה שא"כ אני יכולה להשתמש ב"דיף" הזה ולפי השדות שלו לעשות את השינויים הנדרשים.עליי לעשות לולאה(שמשתמשת ב3 פונקציות ארבע פעמים) ובלולאה זו אני בעצם מבצעת את השינויים הנדרשים. לא לשכוח שעליי לבדוק שבאמת במקום שאני רוצה לבצע שינוי נמצא באמת הביית שציפיתי שיהיה (זה שמופיע אחרי המינוס) - ב"דיף" נמצא בשדה שנקרא אולד
+//if(c1!=c2){
+	//	printf("byte %lu -%u +%u \n", i, c1,c2);
+	
+	//c1 = fgetc(file1);
+	//c2 = fgetc(file2);
+	//++i;
--- Lab3/extendBcmp.c
+++ Lab3/extendBcmp.c
@@ -10,15 +10,9 @@ int main (int argc , char** argv)
 	int c1,c2;
 	FILE* file1;
 	FILE* file2;
-	//char* buff1;
-	//char* buff2;
 	
 	file1 = fopen(argv[1],"r");
 	file2 = fopen(argv[2],"r");
-	//readCount1 = fread(buff1,1, 50,argv[1]);
-	//readCount2 = fread(buff2,1, 50,argv[2]);
-	//fseek(copy_to, write_offset, SEEK_SET);
-	//fseek(copy_from, read_offset, SEEK_SET);
 	
 	i = 1;
 	c1 = fgetc(file1);
@@ -26,7 +20,6 @@ int main (int argc , char** argv)
 	while((c1!=EOF) && (c2!=EOF)){
 	if(c1!=c2){
 		printf("byte %lu -%u +%u \n", i, c1,c2);
-		//break;
 	}
 	c1 = fgetc(file1);
 	c2 = fgetc(file2);
